9deffa212b908909a3ae82566b8837b1386c5598
partouf <partouf@gmail.com>
2017-12-19 20:47:22 +0100

    way to exit

--- checksensor.lua
+++ checksensor.lua
@@ -7,12 +7,20 @@ local shouldQuit = false
 function OnMotion(address, relativeX, relativeY, relativeZ, entityName)
     print("Motion detected\n");
     if (entityName) then
-        print("It's " .. entityName .. "\n")
+        print("It's " .. address .. " @ " .. relativeX .. " x " .. relativeY .. " x " .. relativeZ .. " (" .. entityName .. ")\n")
+    end
+end
+
+-- key_up(keyboardAddress: string, char: number, code: number, playerName: string)
+function OnKeyUp(keyboardAddress, char, code, playerName)
+    if char == 'c' then
+        shouldQuit = true
     end
 end
 
 function Setup()
     event.listen("motion", OnMotion)
+    event.listen()
 end
 
 function MainLoop()
@@ -21,6 +29,8 @@ function MainLoop()
     while not shouldQuit do
         os.sleep(5)
     end
+
+    print("Quit");
 end
 
 Setup()
