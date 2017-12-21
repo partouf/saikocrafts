210206db8c39dee9175eafb7c3a1f5a26ff6eac0
partouf <partouf@gmail.com>
2017-12-21 02:53:27 +0100

    install elev on tablet

--- tablet/tablet.lua
+++ tablet/tablet.lua
@@ -1,3 +1,13 @@
 
+local wget = loadfile("/bin/wget.lua")
 
-print("Hello, world!\n")
+function DownloadFile(url, path)
+    return wget("-fq",url,path)
+end
+
+function UpdateTablet()
+    local url = "https://raw.githubusercontent.com/partouf/saikocrafts/master/tablet/elev.lua"
+    DownloadFile(url, "/home/elev.lua")
+end
+
+UpdateTablet()
