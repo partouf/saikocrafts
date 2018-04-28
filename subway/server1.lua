7400fdc8d7003256a04f71abc47f7fd262e2f860
Partouf <partouf@gmail.com>
2018-04-28 12:34:13 +0200

    logging

--- subway/server1.lua
+++ subway/server1.lua
@@ -13,6 +13,7 @@ local timerSandyPlazaStoplight = -1
 local timerHomeStationStoplight = -1
 
 function rsChanged(address, side, oldValue, newValue, color)
+    print("rsChanged Event!")
     if (side == inputSandyPlazaCart) and (newValue > 0) then
         AlertSandyPlazaCart()
     elseif (side == inputHomeStationCart) and (newValue > 0) then
@@ -41,30 +42,39 @@ function GetHomeStationCartActivity()
 end
 
 function TurnOnHomeStationStopLight()
+    print("TurnOnHomeStationStopLight")
     redstone.setOutput(outputHomeStationStoplight, 255)
     event.timer(30, TurnOffHomeStationStopLight)
 end
 
 function TurnOffHomeStationStopLight()
+    print("TurnOffHomeStationStopLight")
     redstone.setOutput(outputHomeStationStoplight, 0)
     timer = -1
 end
 
 function TurnOnSandyPlazaStopLight()
+    print("TurnOnSandyPlazaStopLight")
     redstone.setOutput(outputSandyPlazaStoplight, 255)
     event.timer(30, TurnOffSandyPlazaStopLight)
 end
 
 function TurnOffSandyPlazaStopLight()
+    print("TurnOffSandyPlazaStopLight")
     redstone.setOutput(outputSandyPlazaStoplight, 0)
     timer = -1
 end
 
 
 function Setup()
-   if event.listen("redstone_changed", rsChanged) == false then
+    term.clear()
+    term.setCursorPos(1,1)
+    term.setCursorBlink(true)
+
+    print("Setup")
+    if event.listen("redstone_changed", rsChanged) == false then
         print("Error registering event");
-   end
+    end
 end
 
 function MainLoop()
